8285fbb261c49e960edb51ca23218e982acfbf07
ArmenG <52843550+ArmenG888@users.noreply.github.com>
2022-12-22 19:18:16 -0800

    Drawing and textarea

--- sticky_note/src/app.js
+++ sticky_note/src/app.js
@@ -1,19 +1,24 @@
+function switch_draw_write(){
+	console.log(document.getElementById('switch').innerText);
+	if(document.getElementById('switch').innerText == "Draw"){
+		document.getElementById('switch').innerHTML = "Write";
+		document.getElementById('note').style.display = "none";
+	}
+	else{
+		document.getElementById('switch').innerHTML = "Draw";
+		document.getElementById('note').style.display = "block";
+	}
+	
+}
 
 var canvas =  document.querySelector("canvas");
 var context = canvas.getContext("2d");
-
-
-
-
 var lastEvent;
 var isMouseDown = false;
-
-
 canvas.addEventListener("mousedown", function(e){
 	lastEvent = e;
 	isMouseDown = true;
 });
-
 canvas.addEventListener("mousemove", function(e){
 	if(isMouseDown) {
 		context.beginPath();
