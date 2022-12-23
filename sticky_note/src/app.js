6ddbffdbe384bc57b58defea5e17d77231f3f9b6
ArmenG <52843550+ArmenG888@users.noreply.github.com>
2022-12-22 18:56:13 -0800

    Started app and drawing

---
+++ sticky_note/src/app.js
@@ -0,0 +1,30 @@
+
+var canvas =  document.querySelector("canvas");
+var context = canvas.getContext("2d");
+
+
+
+
+var lastEvent;
+var isMouseDown = false;
+
+
+canvas.addEventListener("mousedown", function(e){
+	lastEvent = e;
+	isMouseDown = true;
+});
+
+canvas.addEventListener("mousemove", function(e){
+	if(isMouseDown) {
+		context.beginPath();
+		context.moveTo(lastEvent.offsetX,lastEvent.offsetY);	
+		context.lineTo(e.offsetX,e.offsetY);
+		context.stroke();
+		lastEvent = e;		
+	}
+});
+canvas.addEventListener("mouseup", function(){
+	isMouseDown = false;
+});
+
+
