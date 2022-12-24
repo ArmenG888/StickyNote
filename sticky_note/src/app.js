92cee5c5677ac7b210c09be78c853791289462f5
ArmenG <52843550+ArmenG888@users.noreply.github.com>
2022-12-23 17:37:04 -0800

    added eraser and cleaned up code

--- sticky_note/src/app.js
+++ sticky_note/src/app.js
@@ -1,3 +1,11 @@
+
+var color = "#000000"
+var line_width = 1;
+var canvas =  document.querySelector("canvas");
+var context = canvas.getContext("2d");
+var lastEvent;
+var isMouseDown = false;
+
 function draw(){
 	if(document.getElementById('note').style.display == "block")
 	{
@@ -13,40 +21,39 @@ function write(){
 	}
 	
 }
-
+function erase(){
+	color = "#ffffff";
+	line_width = 10;
+}
 function open_nav(){
-	document.getElementById('draw').style.opacity = 1;
-	document.getElementById('draw').style.transition = "opacity 0.5s "; 
-	document.getElementById('text').style.opacity = 1;
-	document.getElementById('text').style.transition = "opacity 0.5s"; 
-	document.getElementById('palette').style.opacity = 1;
-	document.getElementById('palette').style.transition = "opacity 0.5s"; 
-	document.getElementById('color-button').style.opacity = 1;
-	document.getElementById('color-button').style.transition = "opacity 0.5s"; 
-	
+	var icons = document.getElementsByClassName('icons');
+	for (var i = 0; i < icons.length; ++i) {
+		var item = icons[i];  
+		item.style.opacity = 1;
+		item.style.transition = "opacity 0.5s "; 
+	}
 	document.getElementById('right').src = "icons/left.png";
 	document.getElementById('right').setAttribute('onclick','close_nav()')
 }
 function close_nav(){
-	document.getElementById('draw').style.opacity = 0;
-	document.getElementById('draw').style.transition = "opacity 0.5s "; 
-	document.getElementById('text').style.opacity = 0;
-	document.getElementById('text').style.transition = "opacity 0.5s"; 
-	document.getElementById('palette').style.opacity = 0;
-	document.getElementById('palette').style.transition = "opacity 0.5s"; 
-	document.getElementById('color-button').style.opacity = 0;
-	document.getElementById('color-button').style.transition = "opacity 0.5s"; 
+	var icons = document.getElementsByClassName('icons');
+	for (var i = 0; i < icons.length; ++i) {
+		var item = icons[i];  
+		item.style.opacity = 0;
+		item.style.transition = "opacity 0.5s "; 
+	}
 	document.getElementById('right').src = "icons/right.png";
 	document.getElementById('right').setAttribute('onclick','open_nav()')
 }
-let colorWell;
-const defaultColor = "#000000";
-var color = "#000000"
+function componentToHex(c) {
+	var hex = c.toString(16);
+	return hex.length == 1 ? "0" + hex : hex;
+}
 
 window.addEventListener("load", startup, false);
 function startup() {
-	colorWell = document.querySelector("#color_picker");
-	colorWell.value = defaultColor;
+	let colorWell = document.querySelector("#color_picker");
+	colorWell.value = "#000000";
 	colorWell.addEventListener("input", updateFirst, false);
 	colorWell.addEventListener("change", updateAll, false);
 	colorWell.select();
@@ -54,20 +61,16 @@ function startup() {
 function updateFirst(event) {
 	document.getElementById("color-button").style = "background-color:" + event.target.value;
 	document.getElementById("color-button").style.opacity = 1;
-	color = event.target.value;
+	color = componentToHex(event.target.value);
 }
 
 function updateAll(event) {
 	document.getElementById("color-button").style = "background-color:" + event.target.value;
 	document.getElementById("color-button").style.opacity = 1;
-	color = console.log(event.target.value);
+	color = componentToHex(event.target.value);
 }
 
-var canvas =  document.querySelector("canvas");
-var context = canvas.getContext("2d");
 
-var lastEvent;
-var isMouseDown = false;
 canvas.addEventListener("mousedown", function(e){
 	lastEvent = e;
 	isMouseDown = true;
@@ -76,7 +79,8 @@ canvas.addEventListener("mousemove", function(e){
 	if(isMouseDown) {
 		context.beginPath();
 		context.moveTo(lastEvent.offsetX,lastEvent.offsetY);	
-		context.fillStyle  = color;
+		context.lineWidth = line_width
+		context.strokeStyle  = color;
 		context.lineTo(e.offsetX,e.offsetY);
 		context.stroke();
 		lastEvent = e;		
