b1c51d53e845cd3b156a5885b1c9bb423a4ebc78
ArmenG <52843550+ArmenG888@users.noreply.github.com>
2023-05-29 09:04:59 -0700

    Worked on themes

--- sticky_note/src/js/settings.js
+++ sticky_note/src/js/settings.js
@@ -1,6 +1,23 @@
+
+
+
+
 function save(){
-    let themes = {1:['rgb(255,255,255)','black'], 2:['rgb(13,17,23)','white'], 3:['rgb(255,255,136)','black']}
-    document.body.style.backgroundColor = themes[document.getElementById('theme').value][0];
-    document.body.style.color =  themes[document.getElementById('theme').value][1];
-    
+    const Store = require('electron-store');
+    const store = new Store();
+
+    theme = document.getElementById('theme').value;
+    store.set('theme', theme);
+    console.log(store.get('theme'))
+}
+function themeChange(){
+    if (document.getElementById("flexSwitchCheckChecked").checked == true){
+        let themes = {1:['rgb(255,255,255)','black'], 2:['rgb(13,17,23)','white'], 3:['rgb(255,255,136)','black']}
+        document.body.style.backgroundColor = themes[document.getElementById('theme').value][0];
+        document.body.style.color =  themes[document.getElementById('theme').value][1];
+        document.getElementById("theme").style.color = themes[document.getElementById('theme').value][1];
+        document.getElementById("theme").style.backgroundColor = themes[document.getElementById('theme').value][0];
+        document.getElementById("background_image").style.color = themes[document.getElementById('theme').value][1];
+        document.getElementById("background_image").style.backgroundColor = themes[document.getElementById('theme').value][0];
+    }
 }
