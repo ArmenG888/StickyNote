9a53ee6b01d8b4eb221a784abccaa740c27c9916
ArmenG <52843550+ArmenG888@users.noreply.github.com>
2023-05-31 18:34:50 -0700

    changed icons to svg that change color depending on the theme

--- sticky_note/src/js/theme.js
+++ sticky_note/src/js/theme.js
@@ -1,10 +1,21 @@
 const Store = require('electron-store');
 const store = new Store();
+var sPath = window.location.pathname;
+var sPage = sPath.substring(sPath.lastIndexOf('/') + 1);
+
 
 let themes = {1:['rgb(255,255,255)','black'], 2:['rgb(13,17,23)','white'], 3:['rgb(255,255,136)','black']}
 document.body.style.backgroundColor = themes[store.get('theme')][0];
 document.body.style.color =  themes[store.get('theme')][1];
-document.getElementById("theme").style.color = themes[store.get('theme')][1];
-document.getElementById("theme").style.backgroundColor = themes[store.get('theme')][0];
-document.getElementById("background_image").style.color = themes[store.get('theme')][1];
-document.getElementById("background_image").style.backgroundColor = themes[store.get('theme')][0];
+if (sPage == "settings.html")
+{
+    document.getElementById("close").style.fill = themes[store.get('theme')][1];
+    // document.getElementById("theme").style.color = themes[store.get('theme')][1];
+    // document.getElementById("theme").style.backgroundColor = themes[store.get('theme')][0];
+    // document.getElementById("background_image").style.color = themes[store.get('theme')][1];
+    // document.getElementById("background_image").style.backgroundColor = themes[store.get('theme')][0];
+
+}
+else if (sPage == "index.html"){
+    document.getElementById("top-nav").style.fill = themes[store.get('theme')][1];
+}
